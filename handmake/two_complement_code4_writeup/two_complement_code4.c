05b230cd77270fd4df5fefb3a8202a02e1b0fd1f
QuangHao2011 <secretssrt@gmail.com>
2026-07-03 19:10:41 +0700

    Add files via upload

--- handmake/two_complement_code4_writeup/two_complement_code4.c
+++ handmake/two_complement_code4_writeup/two_complement_code4.c
@@ -5,6 +5,8 @@
 int checker(char *number){
 	if(*number == '\0') return 0;
 
+	if(*number == '-') number++;
+
 	while(*number){
 		if(*number < '0' || *number > '9'){
 			return 1;
@@ -21,8 +23,27 @@ int main(int argc , char **argv){
 
 	int x = atoi(argv[1]);
 
-	if(x == INT_MIN && -x == x){ //x == -2147483648, -2147483648 == -2147483648 -> true
-		printf("Flags\n");
+	int flags = 0;
+
+	__asm__ __volatile__(
+		"movl %[x], %%eax\n\t"
+		"movl %%eax, %%edi\n\t"
+		"negl %%edi\n\t"
+		"cmpl %%edi, %%eax\n\t"
+		"jne 1f\n\t"
+
+		"testl %%eax, %%eax\n\t"
+		"je 1f\n\t" //loại x = 0
+
+		"movl $1, %[flags]\n\t"
+		"1:\n\t"
+		: [flags] "+r"(flags)
+		: [x] "r"(x)
+		: "edx","eax", "cc"
+	);
+
+	if(flags){
+		printf("flags\n");
 	}
 
 	printf("thanks\n");
