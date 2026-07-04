b268d5355f6914d2dc4f2b313f5d166bd314c067
QuangHao2011 <secretssrt@gmail.com>
2026-07-04 17:48:18 +0700

    Add files via upload

---
+++ handmake/two_complement_code7_writeup/two_complement_code7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int checker(char *number){
+	if(*number == '\0') return 0;
+
+	if(*number == '-') number++;
+
+	while(*number){
+		if(*number < '0' || *number > '9'){
+			return 1;
+		}
+		*number++;
+	}
+	return 0;
+}
+
+int main(int argc , char **argv){
+	if(argv[1] == NULL) return -1;
+
+	if(checker(argv[1]) == 1) return -1;
+
+	int x = atoi(argv[1]);
+
+ 	if ((x + x + 2) < x && (unsigned)x > INT_MAX){
+		printf("Flags\n");
+		return 0;
+	}
+	printf("thanks\n");
+	return 0;
+}
+
+
+
